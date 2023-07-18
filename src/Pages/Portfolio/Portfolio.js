3ba00a9d1075d77025baec6558883cfcb0942196
Marine Ellin <marine.ellin@gmail.com>
2023-07-18 10:38:18 +0200

    portfolio completed

--- src/Pages/Portfolio/Portfolio.js
+++ src/Pages/Portfolio/Portfolio.js
@@ -1,5 +1,71 @@
+import { useEffect, useState } from "react";
+import styles from "./Portfolio.module.scss";
+import items from "./portfolioItems";
+
 function Portfolio() {
-  return <div>Portfolio</div>;
+  const [imageIndex, setImageIndex] = useState(0);
+  const [itemIndex, setItemIndex] = useState(0);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (imageIndex < 2) {
+        setImageIndex(imageIndex + 1);
+      } else {
+        setImageIndex(0);
+      }
+    }, 5000);
+  });
+
+  function nextItem() {
+    if (itemIndex < items.length - 1) {
+      setItemIndex(itemIndex + 1);
+    }
+  }
+
+  function backItem() {
+    if (itemIndex > 0) {
+      setItemIndex(itemIndex - 1);
+    }
+  }
+
+  return (
+    <div
+      className={`${styles.portfolioContainer} d-flex flex-row flex-fill align-items-center justify-content-center`}
+    >
+      <i
+        class={` ${
+          itemIndex === 0 ? "font-color-transparent-04" : "font-color-gold"
+        } fa-solid fa-circle-chevron-left`}
+        onClick={() => backItem()}
+      ></i>
+      <div className={styles.portfolioContent}>
+        <div className={styles.title}>{items[itemIndex].title}</div>
+        <div className={styles.description}>{items[itemIndex].description}</div>
+        <div className={styles.imageContainer}>
+          <img
+            src={`./portfolioItems/${items[itemIndex].title}/${items[itemIndex].images[imageIndex]}`}
+            alt=""
+          />
+        </div>
+        <div className="d-flex flex-row space-evenly">
+          <a href={items[itemIndex].code} className={styles.btnCode}>
+            Voir le code
+          </a>
+          <a href={items[itemIndex].website} className={styles.btnWebsite}>
+            Voir le site
+          </a>
+        </div>
+      </div>
+      <i
+        class={` ${
+          itemIndex < items.length - 1
+            ? "font-color-gold"
+            : "font-color-transparent-04"
+        } fa-solid fa-circle-chevron-right`}
+        onClick={() => nextItem()}
+      ></i>
+    </div>
+  );
 }
 
 export default Portfolio;
---
+++ src/Pages/Portfolio/portfolioItems.js
@@ -0,0 +1,39 @@
+const items = [
+  {
+    title: "Quiz-en-Flag",
+    description: "Jeux de drapeaux",
+    images: ["quiz1.JPG", "quiz2.JPG", "quiz3.JPG"],
+    code: "https://github.com/MarineEllin/quiz-en-flag.git",
+    website: "https://quiz-en-flag.netlify.app/",
+  },
+  {
+    title: "Agence Immobilière",
+    description: "Site web vitrine d'une agence immobilière fictive",
+    images: ["agence1.JPG", "agence2.JPG", "agence3.JPG"],
+    code: "https://github.com/MarineEllin/agence-immo.git",
+    website: "https://marineellin-agence-immo.netlify.app/",
+  },
+  {
+    title: "CookChef",
+    description: "Site web fictif de recettes",
+    images: ["cookchef1.JPG", "cookchef2.JPG", "cookchef3.JPG"],
+    code: "https://github.com/MarineEllin/cookchef.git",
+    website: "https://coockchef.netlify.app/",
+  },
+  {
+    title: "Café",
+    description: "Site web vitrine d'un café fictif",
+    images: ["cafe1.JPG", "cafe2.JPG", "cafe3.JPG"],
+    code: "https://github.com/MarineEllin/cafe.git",
+    website: "https://marineellin-cafe.netlify.app/",
+  },
+  {
+    title: "Todo List",
+    description: "Mini application de todo list",
+    images: ["todo1.JPG", "todo2.JPG", "todo3.JPG"],
+    code: "https://github.com/MarineEllin/new-todo-list.git",
+    website: "https://marineellin-todo-list.netlify.app/",
+  },
+];
+
+export default items;
