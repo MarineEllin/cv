aabff0485f5e2f6559fdfac745cfe32979e07db4
MarineEllin <marine.ellin@gmail.com>
2023-10-08 21:00:01 +0200

    update portfolio - include react native quiz en flag mobile app

--- src/Pages/Portfolio/portfolioItems.js
+++ src/Pages/Portfolio/portfolioItems.js
@@ -1,26 +1,37 @@
 const items = [
   {
-    title: "Quiz-en-Flag",
-    description: "Jeux de drapeaux à multi-choix : 1 drapeau, 3 noms de pays !",
+    title: "Quiz-en-Flag - React Native",
+    description:
+      "Application mobile de jeu de drapeaux - Développée en React Native - Publiée sur google store",
+    images: ["quizrn1.jpg", "quizrn2.jpg", "quizrn3.jpg"],
+    code: "https://github.com/MarineEllin/quiz-en-flag-mobile",
+    website:
+      "https://play.google.com/store/apps/details?id=com.mellin.quizenflagmobile&pcampaignid=web_share&pli=1",
+  },
+  {
+    title: "Quiz-en-Flag - React",
+    description:
+      "Application web de jeu de drapeaux - Développée en React - Déployée avec Netlify",
     images: ["quiz1.JPG", "quiz2.JPG", "quiz3.JPG"],
     code: "https://github.com/MarineEllin/quiz-en-flag.git",
     website: "https://quiz-en-flag.netlify.app/",
   },
   {
-    title: "Agence Immobilière",
-    description: "Site web vitrine d'une agence immobilière fictive",
-    images: ["agence1.JPG", "agence2.JPG", "agence3.JPG"],
-    code: "https://github.com/MarineEllin/agence-immo.git",
-    website: "https://marineellin-agence-immo.netlify.app/",
-  },
-  {
     title: "CookChef",
     description:
-      "Site web fictif de recettes avec wishlist et administration des recettes",
+      "Site web de recettes avec wishlist et administration des recettes - React, TypeScript, Sass",
     images: ["cookchef1.JPG", "cookchef2.JPG", "cookchef3.JPG"],
     code: "https://github.com/MarineEllin/cookchef.git",
     website: "https://coockchef.netlify.app/",
   },
+  {
+    title: "Agence Immobilière",
+    description: "Site web vitrine d'une agence immobilière - HTML & CSS",
+    images: ["agence1.JPG", "agence2.JPG", "agence3.JPG"],
+    code: "https://github.com/MarineEllin/agence-immo.git",
+    website: "https://marineellin-agence-immo.netlify.app/",
+  },
+
   {
     title: "Café",
     description:
@@ -28,14 +39,6 @@ const items = [
     images: ["cafe1.JPG", "cafe2.JPG", "cafe3.JPG"],
     code: "https://github.com/MarineEllin/cafe.git",
     website: "https://marineellin-cafe.netlify.app/",
-  },
-  {
-    title: "Todo List",
-    description:
-      "Mini application de todo list : mon premier projet avec JavaScript",
-    images: ["todo1.JPG", "todo2.JPG", "todo3.JPG"],
-    code: "https://github.com/MarineEllin/new-todo-list.git",
-    website: "https://marineellin-todo-list.netlify.app/",
   },
 ];
 
